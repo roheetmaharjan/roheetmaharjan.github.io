a119dfc3ac920fd9aafce78c15ab348776a7b9be
Rohit Maharjan <swift.rohit13@gmail.com>
2017-06-13 20:35:14 +0545

    new

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ jQuery(document).ready(function(){
         
     })
 })
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
+
+$('.custom1').owlCarousel({
+    animateOut: 'slideOutDown',
+    animateIn: 'flipInX',
+    items:1,
+    margin:30,
+    stagePadding:30,
+    smartSpeed:450
+});
