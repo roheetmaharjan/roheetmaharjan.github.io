941108e0eabd7e34a3866533b7661dd56484aeee
Rohit Maharjan <swift.rohit13@gmail.com>
2017-09-03 08:18:54 +0545

    new

--- js/main.js
+++ js/main.js
@@ -1,26 +1,5 @@
-jQuery(document).ready(function(){
-    
-    jQuery('.burger-nav').click(function(e){
-        
-        jQuery('nav ul').toggleClass('navbar-collapse')
-        
-        e.preventDefault()
-        
-    })
-})
-jQuery(document).ready(function(){
-	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},6000);
-	});
-});
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('particles.json loaded...');
+  });
 
-$('.custom1').owlCarousel({
-    animateOut: 'slideOutDown',
-    animateIn: 'flipInX',
-    items:1,
-    margin:30,
-    stagePadding:30,
-    smartSpeed:450
-});
+  $('.banner-wrap').parallax({imageSrc: '../images/banner.jpg'});
