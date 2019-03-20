34dd3537521d99194a32d3fb157fc5b9a0d9e7f8
Rohit Maharjan <swift.rohit13@gmail.com>
2019-03-20 07:40:48 +0545

    responsive changes

--- js/main.js
+++ js/main.js
@@ -56,7 +56,26 @@ $(document).ready(function () {
     autoplaySpeed: 6000,
     speed: 800,
     slidesToShow: 2,
-    adaptiveHeight: true
+    adaptiveHeight: true,
+    responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
   });
   var scroll = $(window).scrollTop();
 
